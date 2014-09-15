005f91398291b50ccfbbed83147f04b3635ba3cb
Sean Gilligan <sean@msgilligan.com>
2014-09-15 10:34:23 -0700

    Ubuntu/Upstart init scripts for Master Core
    Back-ported some files from BitCoin core (approved) PR’s with some
    additions for Master Core.
    
    See:
    https://github.com/bitcoin/bitcoin/issues/4124
    https://github.com/bitcoin/bitcoin/pull/4611

---
+++ contrib/msc-ubuntu/README.md
@@ -0,0 +1,20 @@
+Master Core Upstart support for Ubuntu (tested on 14.04 LTS)
+
+This is based upon upstream work that is documented in these links:
+
+* [Issue #4124](https://github.com/bitcoin/bitcoin/issues/4124)
+* [Issue #4611](https://github.com/bitcoin/bitcoin/pull/4611)
+
+This branch/PR used "git cherry-pick" to pull some of that code so it can be added to the michael-0921 branch of Master Core.
+
+Doc on the files from the "Cherry Pick" can be found:
+
+* [init/README.md](../init/README.md)
+* [doc/init.md](../../doc/init.md)
+
+It has been tested on Ubuntu 14.04 LTS.
+
+There is a Vagrant script to install it here:
+[https://github.com/msgilligan/install-msc/tree/vagrant](https://github.com/msgilligan/install-msc/tree/vagrant)
+
+
---
+++ contrib/msc-ubuntu/install-mastercore-upstart.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Copy the executable
+cp src/bitcoind /usr/bin/bitcoind
+
+# Set up directories, users, and permissions as standardized by Bitcoin Core
+mkdir -p /etc/bitcoin
+mkdir -p /var/lib/bitcoind
+mkdir -p /var/run/bitcoind
+
+groupadd --force --system  bitcoin
+useradd -M --system --shell /usr/sbin/nologin --home /var/lib/bitcoind --gid bitcoin bitcoin
+
+chown bitcoin:bitcoin /var/lib/bitcoind
+chmod 750 /var/lib/bitcoind
+
+chown bitcoin:bitcoin /var/run/bitcoind
+chmod 755 /var/run/bitcoind
+
+# Copy Bitcoin configuration file
+cp contrib/msc-ubuntu/bitcoin.conf /etc/bitcoin
+chown bitcoin:bitcoin /etc/bitcoin/bitcoin.conf
+chmod 660 /etc/bitcoin/bitcoin.conf
+
+# Copy Upstart configuration file/script
+cp contrib/init/bitcoind.conf /etc/init
+
+echo "Set rpcuser and rpcpassword in /etc/bitcoin/bitcoin.conf, then type: service bitcoind start"
+
